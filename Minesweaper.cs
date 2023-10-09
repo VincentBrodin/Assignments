9fe03cc9f2b1d6e5fd1418d027a11cc43ba93475
Vincent Brodin <64703732+VincentBrodin@users.noreply.github.com>
2023-10-09 16:21:31 +0200

    Update Minesweaper.cs

--- Minesweaper.cs
+++ Minesweaper.cs
@@ -1,215 +1,194 @@
-namespace Assignment2;
+namespace Struct;
 
 internal static class Program
 {
     //SETTINGS
-    private const string StartNotation = "1/3/5/7";
-    
+    private const int Width = 9;
+    private const int Height = 9;
+    private const string Space = " "; 
+    private const int BombCount = 18;
     
     private static void Main(string[] args)
     {
-        //Get players
-        int playerCount = GetInput("How many players? (1/2)", 1, 2);
-        int currentPlayerIndex = 0;
-        Player[] players = new Player[2];
-        if(playerCount == 1)
+        Tile[,] tiles = new Tile[Width+2,Height+2];
+        for (int x = 0; x < Width+2; x++)
         {
-            Console.WriteLine("Enter player 1 name");
-            string player1 = Console.ReadLine() ?? "Player 1";
-            players[0] = new Player(player1, false);
-            players[1] = new Player("AI", true);
-        }
-        else
-        {
-            Console.WriteLine("Enter player 1 name");
-            string player1 = Console.ReadLine() ?? "Player 1";
-            Console.WriteLine("Enter player 2 name");
-            string player2 = Console.ReadLine() ?? "Player 2";
-            
-            players[0] = new Player(player1, false);
-            players[1] = new Player(player2, false);
-        }
-        
-        Console.Clear();
-        
-        //Get start notation
-        string startNotation = StartNotation;
-        Console.WriteLine("Do you want to enter your own start notation? (y/n)");
-        string? input = Console.ReadLine();
-        if (input == "y")
-        {
-            Console.WriteLine("Enter your start notation");
-            startNotation = Console.ReadLine() ?? StartNotation;
-        }
-        
-        
-        //Init piles
-        (int pileCount, int maxSticks) = BreakUpNotation(startNotation);
-        
-        Pile[] piles = new Pile[pileCount];
-        for (int i = 0; i < piles.Length; i++)
-        {
-            piles[i] = new Pile(maxSticks);
-            for (int j = 0; j < piles[i].Sticks.Length; j++)
+            for (int y = 0; y < Height + 2; y++)
             {
-                piles[i].Sticks[j] = new Stick(true);
+                tiles[x,y] = new Tile(x,y);
+                if(x == 0 || y == 0 || x == Width+1 || y == Height+1)
+                    tiles[x,y].IsWall = true;
+                else 
+                    tiles[x,y].IsWall = false;
+            }
+        }
+
+        Random random = new Random();
+        for (int i = 0; i < BombCount; i++)
+        {
+            int x = random.Next(1, Width);
+            int y = random.Next(1, Height);
+            if (!tiles[x, y].IsBomb)
+            {
+                tiles[x, y].IsBomb = true;
             }
         }
         
-        //Apply start notation
-        piles.SetNotation(startNotation);
-        
-        Console.Clear();
-        //Game Loop
         while (true)
         {
-            Player currentPlayer = players[currentPlayerIndex];
             Console.Clear();
-            Console.WriteLine(currentPlayer.Name + "'s turn");
-            piles.Print();
-
-            int chosenPile = 0, stickAmount = 0;
-            if (currentPlayer.IsAi)
-            {
-                (chosenPile, stickAmount) = CalculateAiMove(piles.GetNotation());
-            }
-            else
-            {
-                //Get pile
-                chosenPile = GetInput("Pick a pile", 1, piles.Length) - 1;
+            tiles.UpdateBoard();
+            tiles.PrintGrid();
+            Console.WriteLine("Enter X:");
+            string? xInput = Console.ReadLine();
+            if (!int.TryParse(xInput, out int x))
+                continue;
+            Console.WriteLine("Enter Y:");
+            string? yInput = Console.ReadLine();
+            if (!int.TryParse(yInput, out int y))
+                continue;
             
-                //Get sticks
-                stickAmount = GetInput("Pick a number of sticks", 1, piles[chosenPile].Sticks.Count(stick => !stick.IsTaken));
-                
-            }
-
-            piles[chosenPile].RemoveSticks(stickAmount);
+            if(!(y is > 0 and < Height+1 && x is > 0 and < Height+1))
+                continue;
             
-            if (piles.All(pile => pile.Sticks.All(stick => stick.IsTaken)))
+            Console.WriteLine("enter action (r/f)");
+            string? action = Console.ReadLine();
+            
+            if (action == "r")
             {
-                break;
+                tiles[x, y].IsRevealed = true;
             }
-            
-            currentPlayerIndex = (currentPlayerIndex + 1) % 2;
+            else if (action == "f")
+            {
+                tiles[x, y].IsFlagged = true;
+            }
         }
     }
 
-    private static (int,int) CalculateAiMove(string notation)
+    private static void UpdateBoard(this Tile[,] tiles)
     {
-        //TODO: Implement AI
-        return (0, 0);
-    }
-
-    private class Stick(bool isTaken)
-    {
-        public bool IsTaken = isTaken;
-    }
-    
-    private class Pile(int maxSticks)
-    {
-        public readonly Stick[] Sticks = new Stick[maxSticks];
-
-        public void Print()
+        for(int x = 1; x < Width+1; x++)
         {
-            foreach (var stick in Sticks)
+            for (int y = 1; y < Height+1; y++)
             {
-                if(!stick.IsTaken)
-                    Console.Write("|");
+                Tile tile = tiles[x, y];
+                if (tile.IsBomb)
+                {
+                    continue;
+                }
+
+                int bombCount = 0;
+                for (int x1 = x - 1; x1 <= x + 1; x1++)
+                {
+                    for (int y1 = y - 1; y1 <= y + 1; y1++)
+                    {
+                        if (tiles[x1, y1].IsBomb)
+                        {
+                            bombCount++;
+                        }
+                    }
+                }
+                tiles[x,y].BombCount = bombCount;
+
+                if (bombCount == 0 && tile.IsRevealed)
+                {
+                    for (int x1 = x - 1; x1 <= x + 1; x1++)
+                    {
+                        for (int y1 = y - 1; y1 <= y + 1; y1++)
+                        {
+                            tiles[x1, y1].IsRevealed = true;
+                        }
+                    }
+                }
             }
+        }
+    }
+
+    private static void PrintGrid(this Tile[,] tiles)
+    {
+        Console.ForegroundColor = ConsoleColor.Blue;
+        Console.WriteLine("X");
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine("Y");
+        Console.ForegroundColor = ConsoleColor.White;
+        for (int x = 0; x < Width+2; x++)
+        {
+            for (int y = 0; y < Height+2; y++)
+            {
+                Tile tile = tiles[x, y];
+                if (tile.IsWall)
+                {
+                    Console.BackgroundColor = ConsoleColor.DarkGray;
+                    if (x == 0 && y is > 0 and < Height+1)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.Write($"|{Space}{y}{Space}|");
+                        Console.ForegroundColor = ConsoleColor.White;
+
+                    }
+                    else if (y == 0 && x is > 0 and < Width+1)
+                    { 
+                        Console.ForegroundColor = ConsoleColor.Blue;
+                        Console.Write($"|{Space}{x}{Space}|");
+                        Console.ForegroundColor = ConsoleColor.White;
+                    }
+                    else
+                    {
+                        Console.BackgroundColor = ConsoleColor.Gray;
+                        Console.Write($"|{Space}#{Space}|");
+                    }
+                    Console.BackgroundColor = ConsoleColor.Black;
+                }
+                else if (tile.IsFlagged)
+                {
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.Write($"|{Space}|{Space}|");
+                    Console.ForegroundColor = ConsoleColor.White;
+                }
+                else if (tile.IsRevealed)
+                {
+                    if (tile.IsBomb)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.Write($"|{Space}*{Space}|");
+                        Console.ForegroundColor = ConsoleColor.White;
+                    }
+                    else if (tile.BombCount != 0)
+                    {
+                        switch (tile.BombCount)
+                        {
+                            case 1:
+                                Console.ForegroundColor = ConsoleColor.Blue;
+                                break;
+                            case 2:
+                                Console.ForegroundColor = ConsoleColor.Green;
+                                break;
+                            default:
+                                Console.ForegroundColor = ConsoleColor.Red;
+                                break;
+                        }
+                        Console.Write($"|{Space}{tile.BombCount}{Space}|");
+                        Console.ForegroundColor = ConsoleColor.White;
+                    }
+                    else
+                    {
+                        Console.Write($"|{Space} {Space}|");
+                    }
+                }
+                else 
+                {
+                    Console.Write($"|{Space}X{Space}|");
+                }
+            }
+
             Console.WriteLine();
         }
-        
-        public void RemoveSticks(int amount)
-        {
-            int taken = 0;
-            foreach (var stick in Sticks)
-            {
-                if (!stick.IsTaken)
-                {
-                    stick.IsTaken = true;
-                    taken++;
-                }
-                
-                if(taken == amount)
-                    return;
-            }
-        }
     }
-    
-    private class Player(string name, bool isAi)
+
+    public static ConsoleColor HexToColor(string hex)
     {
-        public readonly string Name = name;
-        public bool IsAi = isAi;
+        int number = Convert.ToInt32(hex, 16);
+        ConsoleColor color = (ConsoleColor)number;
+        return color;
     }
-
-    #region Extensions
-    private static void SetNotation(this Pile[] piles, string notation)
-    {
-        string[] states = notation.Split("/");
-        for (int i = 0; i < states.Length; i++)
-        {
-            int sticksCount = int.Parse(states[i]);
-            for (int j = 0; j < sticksCount; j++)
-            {
-                piles[i].Sticks[j].IsTaken = false;
-            }
-        }
-    }
-    
-    private static string GetNotation(this Pile[] piles)
-    {
-        int pileCount = piles.Length;
-        string[] states = new string[pileCount];
-        for (int i = 0; i < pileCount; i++)
-        {
-            states[i] = piles[i].Sticks.Count(stick => !stick.IsTaken).ToString();
-        }
-        
-        string notation = string.Join("/", states);
-        
-        return notation;
-    }
-    
-    private static void Print(this Pile[] piles)
-    {
-        for (var i = 0; i < piles.Length; i++)
-        {
-            var pile = piles[i];
-            Console.Write(i + 1 + ": ");
-            pile.Print();
-        }
-    }
-    #endregion
-    
-    private static (int,int) BreakUpNotation(string notation)
-    {
-
-        string[] states = notation.Split("/");
-
-        int piles = states.Length;
-
-        int maxSticks = states.Select(int.Parse).Prepend(0).Max();
-
-        return (piles, maxSticks);
-    }
-    
-    private static int GetInput(string message, int min, int max)
-    {
-        while (true)
-        {
-            Console.WriteLine(message);
-            string? input = Console.ReadLine();
-            if (int.TryParse(input, out int result))
-            {
-                if (result >= min && result <= max)
-                {
-                    return result;
-                }
-            }
-        }
-    }
-
-
-    
 }
-
